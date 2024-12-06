49107fe4df59eb9103df295cccd617588b72aefe
Ryan <le_ryan@hotmail.com>
2024-12-06 10:56:44 -0600

    fix: update azure credentials

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,11 +1,12 @@
 <?php
+require_once 'azure_config.php';
 
 try {
     // Connection parameters
-    $host = "rle-server.mysql.database.azure.com";
-    $username = "bckcctbcay";
-    $password = 'yBJedelxw12Vb$dK';
-    $database = "rle-database";
+    $host = AZURE_MYSQL_HOST;
+    $username = AZURE_MYSQL_USERNAME;
+    $password = AZURE_MYSQL_PASSWORD;
+    $database = AZURE_MYSQL_DBNAME;
     $dsn = "mysql:host=$host;dbname=$database;port=3306;charset=utf8mb4";
 
     // SSL Certificate Path
