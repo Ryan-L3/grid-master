3cce1b9d5cba3c385d12c7b7430a303333545abd
Ryan <le_ryan@hotmail.com>
2024-12-06 13:06:59 -0600

    fix: dev branch for local mysql server

--- database/db_connect.php
+++ database/db_connect.php
@@ -2,10 +2,10 @@
 
 try {
     // Connection parameters
-    $host = getenv('AZURE_MYSQL_HOST');
-    $username = getenv('AZURE_MYSQL_USERNAME');
-    $password = getenv('AZURE_MYSQL_PASSWORD');
-    $database = getenv('AZURE_MYSQL_DBNAME');
+    $host = "rle-server.mysql.database.azure.com";
+    $username = "bckcctbcay";
+    $password = 'yBJedelxw12Vb$dK';
+    $database = "rle-database";
     $dsn = "mysql:host=$host;dbname=$database;port=3306;charset=utf8mb4";
 
     // SSL Certificate Path
@@ -22,4 +22,4 @@ try {
     die("Connection failed: " . $e->getMessage());
 }
 
-?>`
+?>
